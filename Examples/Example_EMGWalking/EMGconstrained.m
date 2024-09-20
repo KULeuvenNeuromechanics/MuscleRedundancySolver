1172c4f30a9007110542453003a99810175fac6f
KULeuvenNeuromechanics <67641878+KULeuvenNeuromechanics@users.noreply.github.com>
2024-09-20 16:01:21 +0200

    Update EMGconstrained.m

--- Examples/Example_EMGWalking/EMGconstrained.m
+++ Examples/Example_EMGWalking/EMGconstrained.m
@@ -13,6 +13,7 @@ Misc.IDfile  = {fullfile(pwd,'ID_gait.sto')};
 Misc.EMGfile = {fullfile(pwd,'EMG_gait.mot')};
 Misc.model_path   = {fullfile(pwd,'ScaledModel.osim')};
 Misc.OutPath      = fullfile(pwd,'Results_LowerLimb_EMGconstrained');                    % folder to store results
+Misc.AnalysisID = 'v1'; % a name for the analysis
 time = [1.2 2.3]; 
 
 %% Settings
